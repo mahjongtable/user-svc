a27a4664af3f8f509679fe866139b95397f1982a
hjkl1 <1161524405@qq.com>
2025-05-14 16:32:32 +0800

    Add a error in the repository layer

--- src/db/repository.rs
+++ src/db/repository.rs
@@ -1,35 +1,30 @@
 use async_trait::async_trait;
 use sqlx::Error;
 
-use super::entity::{CreateUser, UserEntity};
-
-// pub trait User: Send {
-//     fn uid(&self) -> Option<String>;
-//     fn username(&self) -> Option<String>;
-//     fn gender(&self) -> Option<i32>;
-//     fn avatar_url(&self) -> Option<String>;
-//     fn email(&self) -> String;
-//     fn cellphone_number(&self) -> Option<String>;
-//     fn password(&self) -> String;
-// }
-
-// #[derive(Debug)]
-// pub struct User {
-//     pub id: Option<u64>,
-//     pub username: Option<String>,
-//     pub gender: Option<i8>,
-//     pub avatar_url: Option<String>,
-//     pub email: String,
-//     pub cellphone_number: Option<String>,
-//     pub password: Option<String>,
-//     pub created_at: Option<chrono::NaiveDateTime>,
-//     pub updated_at: Option<chrono::NaiveDateTime>,
-//     pub deleted_at: Option<chrono::NaiveDateTime>,
-// }
+use super::entity::{CreateUser, UpdateUser, UserEntity};
 
 #[async_trait]
 pub trait UserRepository: Send + Sync {
     async fn get_user(&self, uid: u64) -> Result<UserEntity, Error>;
     async fn create_user(&self, user: CreateUser) -> Result<u64, Error>;
     async fn delete_user(&self, uid: u64) -> Result<(), Error>;
+    async fn update_user(&self, uid: u64, user: UpdateUser) -> Result<(), RepositoryError>;
+}
+
+#[derive(Debug, thiserror::Error)]
+pub enum RepositoryError {
+    #[error("The record does not found.")]
+    RecordNotFound,
+
+    #[error("The database error: {0}")]
+    Database(Box<dyn std::error::Error + Send + Sync>),
+}
+
+impl From<sqlx::Error> for RepositoryError {
+    fn from(value: sqlx::Error) -> Self {
+        match value {
+            sqlx::Error::RowNotFound => RepositoryError::RecordNotFound,
+            other_err => RepositoryError::Database(Box::new(other_err)),
+        }
+    }
 }
